2c878e87c3d3ac1c0ab10d586ac03b5087a920bf
mohamed korany <>
2021-07-16 13:45:20 +0200

    initial commit

---
+++ AsgatechNewsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsgatechNewsTask
+//
+//  Created by Mohamed Korany on 7/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
